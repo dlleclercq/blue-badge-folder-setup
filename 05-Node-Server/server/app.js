fe6817794b74985983ef6b2fbb5c2c13b1d7d94c
David LeClercq <dleclercq289@gmail.com>
2021-07-29 17:50:33 -0400

    fixed my files on the forked blue badge modules

---
+++ 05-Node-Server/server/app.js
@@ -0,0 +1,15 @@
+let express = require("express");
+const app = express();
+const sequelize = require("./db");
+let journal = require("./controllers/journalcontroller");
+let user = require("./controllers/usercontroller");
+
+sequelize.sync();
+
+app.use(express.json());
+app.use('/journal', journal);
+
+app.use("/user", user);
+app.listen(3000, function () {
+    console.log("App is listening on port 3000")
+});
